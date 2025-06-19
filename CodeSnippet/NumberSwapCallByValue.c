67cdfed6e3710bcfbfb7ae1d4032254d87c5fb27
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-06-19 18:03:28 +0530

    new snippets

---
+++ CodeSnippet/NumberSwapCallByValue.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+void swapNumber(int a ,int b);
+void main(){
+    int x,y;
+    printf("Enter the number x: ");
+    scanf("%d",&x);
+    printf("Enter the number y: ");
+    scanf("%d",&y);
+    printf("Before Swapping x is %d and y is %d \n",x,y);
+    swapNumber(x,y);
+    printf("After Swapping x is %d and y is %d \n",x,y);
+}
+void swapNumber(int a, int b){
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+
+}
