3671f834a7b2231c6b94ec02566208de9a107f03
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-11-01 01:26:09 +0530

    Added Data structures Folders

---
+++ Data-Structures/Lecture3/If-Else_Practise.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello wolrd" << endl;
+
+    // If Else
+
+    // int a;
+    // cout << "Enter The Number :" << endl;
+    // cin >> a;
+    // if (a % 2 == 0)
+    // {
+    //     cout << "Number is Even" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Number is Odd" << endl;
+    // }
+
+    // While Loop
+
+    // int number;
+    // cout << "Enter the number: ";
+    // cin >> number;
+    // int i = 0;
+    // while (i <= 10)
+    // {
+    //     cout << number << "X" << i << "=" << number * i <<endl;
+    //     i++;
+    // }
+
+    //  Question:  Sum of N Numbers
+
+    int number;
+    cout << "Enter the Number :";
+    cin >> number;
+    int sum = 0;
+    int i = 1;
+    while (i <= number)
+    {
+        if (i % 2 == 0)
+        {
+            sum = sum + i;
+        }
+        i++;
+    }
+    cout << "Sum is " << sum << endl;
+
+    return 0;
+}
