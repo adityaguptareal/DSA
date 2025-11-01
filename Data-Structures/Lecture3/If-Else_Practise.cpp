f9eccef44011ab529e968b56c4d5a53130ad082f
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-11-01 12:55:32 +0530

    If Else practise

--- Data-Structures/Lecture3/If-Else_Practise.cpp
+++ Data-Structures/Lecture3/If-Else_Practise.cpp
@@ -33,20 +33,44 @@ int main()
 
     //  Question:  Sum of N Numbers
 
-    int number;
-    cout << "Enter the Number :";
-    cin >> number;
-    int sum = 0;
+    // int number;
+    // cout << "Enter the Number :";
+    // cin >> number;
+    // int sum = 0;
+    // int i = 1;
+    // while (i <= number)
+    // {
+    //     if (i % 2 == 0)
+    //     {
+    //         sum = sum + i;
+    //     }
+    //     i++;
+    // }
+    // cout << "Sum is " << sum << endl;
+
+    // Pattern Prining Problem
+
+    // Pattern 1 Question
+
+    //  ****
+    //  ****
+    //  ****
+    //  ****
+    int n;
+    cout << "Enter the Number : ";
+    cin >> n;
     int i = 1;
-    while (i <= number)
+    while (i <= n)
     {
-        if (i % 2 == 0)
+        int j = 1;
+        while (j <= n)
         {
-            sum = sum + i;
+            cout << "*";
+            j++;
         }
+        cout << endl;
         i++;
     }
-    cout << "Sum is " << sum << endl;
 
     return 0;
 }
