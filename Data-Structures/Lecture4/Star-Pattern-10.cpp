880eea5816b0c2a6334ea2a8d925a8cb23494ea2
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-11-05 23:39:50 +0530

    Pattern 10

---
+++ Data-Structures/Lecture4/Star-Pattern-10.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Pattern 10
+    // A A A
+    // B B B
+    // C C C
+    // D D D
+
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    int row = 1;
+
+    while (row <= n)
+    {
+        int column = 1;
+        while (column <= n)
+        {
+            char output = ('A' + row - 1);
+            cout << output << " ";
+            column++;
+        }
+        cout << endl;
+        row++;
+    }
+}
