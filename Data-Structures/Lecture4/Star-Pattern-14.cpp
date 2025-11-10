97077aa584d778098cf8ca02de88c3225516ec5f
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-11-10 23:57:35 +0530

    Pattern 14 completed

---
+++ Data-Structures/Lecture4/Star-Pattern-14.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Pattern 14
+    // A 
+    // B B
+    // C C C
+
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    int row = 1;
+    while (row <= n)
+    {
+
+        int column = 1;
+        while (column <= row)
+        {
+            char output = ('A' + row - 1);
+            cout << output << " ";
+            column++;
+        }
+        cout << endl;
+        row++;
+    }
+}
