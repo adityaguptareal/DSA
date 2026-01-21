0860b600829b5bc69ea03fe68c6bacaed9d199e7
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2026-01-21 12:00:33 +0530

    Initial commit

---
+++ C++Baisc.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    std::cout << "Printed \n"; 
+    std::cout << "Aditya"; 
+
+    cout<<" \n Testing \n";
+
+    int x,y;
+    cin >>x>>y;
+    cout<<"X is"<< x << "and y is" << y ;
+
+
+    return 0;
+}
