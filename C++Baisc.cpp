5c9951e22646e72166ecbca919df519a1c516922
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2026-01-21 22:55:08 +0530

    DSA lecture 1

--- C++Baisc.cpp
+++ C++Baisc.cpp
@@ -1,16 +1,91 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    std::cout << "Printed \n"; 
-    std::cout << "Aditya"; 
+void display(string name)
+{
+    cout << "Hi" << " " << name;
+}
 
-    cout<<" \n Testing \n";
+int main()
+{
+    // std::cout << "Printed \n";
+    // std::cout << "Aditya";
 
-    int x,y;
-    cin >>x>>y;
-    cout<<"X is"<< x << "and y is" << y ;
+    // cout << " \n Testing \n";
 
+    // int x, y;
+    // cin >> x >> y;
+    // cout << "X is" << x << "and y is" << y;
+
+    // long long p = 1500000;
+    // float f = 5.6;
+    // float h = 5.3;
+
+    // string name;
+    // cout << "Enter your name: ";
+    // cin >> name;
+    // cout << "Hi" << " " << name << endl;
+
+    // string string;
+    // getline(cin, string);
+    // cout << "Text is " << string;
+
+    // int number;
+    // cout << name << " " << "Enter your age: ";
+    // cin >> number;
+    // switch (number)
+    // {
+    // case 1:
+    //     cout << "Number is 1";
+    //     break;
+    // case 2:
+    //     cout << "Number is 2";
+    //     break;
+    // default:
+    //     break;
+    // }
+    // if (number > 18)
+    // {
+    //     cout << "You are eligible to vote";
+    // }
+    // else
+    // {
+    //     cout << "You are not eligible to vote";
+    // }
+
+    // int arr[10];
+    // arr[0] = 1;
+    // cout <<"First element is " << arr[0];
+
+    // 2D array
+    // int arr[3][5];
+    // arr[1][3] = 65;
+    // cout << arr[1][3];
+
+    // string yourName = "Aditya";
+    // length to get the length
+    // for (int i = 0; i < yourName.length(); i++)
+    // {
+    //     cout << "YourName " << "For " << i + 1
+    //     << endl;
+    // }
+    // yourName[4] = 'A';
+    // cout << yourName;
+
+    // for (int i = 0; i <= 10; i++)
+    // {
+    //     cout << "Aditya " << i + 1 << endl;
+    //     if(i==4) break;
+    // }
+
+    // int j = 1;
+    // do
+    // {
+    //     cout << "Hello\n" ;
+    //     j++;
+    // } while (j <= 10);
+
+    display("Aditya");
 
     return 0;
 }
