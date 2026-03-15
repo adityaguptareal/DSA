3eb56b8cf1806a00030dbfda5c75e8c39f441610
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2026-03-16 03:50:35 +0530

    selection sort algorithm

---
+++ sorting/selection-sort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        int index = i;
+
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[index])
+            {
+                index = j;
+            }
+        }
+
+        swap(arr[i], arr[index]);
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    cout << "Sorted Array: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+    int arr[5] = {4, 5, 2, 1, 3};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    selectionSort(arr, size);
+    printArray(arr, size);
+
+    return 0;
+}
