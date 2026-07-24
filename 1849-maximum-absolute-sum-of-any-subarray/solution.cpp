d79bba00dd8ab35d8f123e31ac3d80eaef668d9b
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2026-07-24 10:40:05 +0000

    [Aditya Kumar] Runtime - 0 ms (100.00%), Memory - 45.1 MB (61.62%)

--- 1849-maximum-absolute-sum-of-any-subarray/solution.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/solution.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
-        int maxSum = nums[0];
         int minSum = nums[0];
+        int maxSum = nums[0];
         int bestMinSum = nums[0];
         int bestMaxSum = nums[0];
-
         int result = nums[0];
+      
         for (int i = 1; i < nums.size(); i++) {
-
-            maxSum = max(maxSum + nums[i], nums[i]);
-            bestMaxSum = max(bestMaxSum, maxSum);
             minSum = min(minSum + nums[i], nums[i]);
             bestMinSum = min(bestMinSum, minSum);
+            maxSum = max(maxSum + nums[i], nums[i]);
+            bestMaxSum = max(bestMaxSum, maxSum);
         }
-
-        return max(abs(bestMaxSum), abs(bestMinSum));
+        return max(abs(bestMinSum),bestMaxSum);
     }
 };
