83b2c305a43155c16474393f55fa6f01d509e9ee
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2026-07-26 11:21:57 +0000

    [Aditya Kumar] Runtime - 3 ms (19.72%), Memory - 35.8 MB (29.66%)

---
+++ 0724-find-pivot-index/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int left = 0;
+        int sum = 0;
+
+
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+        }
+        if (sum - nums[0] == 0) {
+            return 0;
+        }
+        for (int i = 1; i < nums.size(); i++) {
+            left += nums[i - 1];
+            int right = sum - left - nums[i];
+            if (left == right) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
