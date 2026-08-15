e0a5941e8a51b56aece82153e6dce6c2a06aa354
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2026-08-15 17:49:01 +0000

    [Aditya Kumar] Runtime - 0 ms (100.00%), Memory - 19.4 MB (65.51%)

--- 0167-two-sum-ii---input-array-is-sorted/solution.cpp
+++ 0167-two-sum-ii---input-array-is-sorted/solution.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int i = 0;
-        int j = numbers.size() - 1;
-        while (i < j) {
-            if (numbers[i] + numbers[j] == target) {
-                return {i + 1, j + 1};
-            } else if (numbers[i] + numbers[j] > target) {
-                j--;
+        int left = 0;
+        int right = numbers.size() - 1;
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target) {
+                return {left + 1, right + 1};
+            } else if (numbers[left] + numbers[right] < target) {
+                left++;
             } else {
-                i++;
+                right--;
             }
         }
         return {};
