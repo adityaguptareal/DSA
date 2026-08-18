8b39f29762775f3579bc7a28e51bc1baf7005025
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2026-08-18 21:04:04 +0000

    [Aditya Kumar] Runtime - 3 ms (30.16%), Memory - 23 MB (7.35%)

--- 0057-insert-interval/solution.cpp
+++ 0057-insert-interval/solution.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-
-        vector<vector<int>> res;
-
-        int start1 = newInterval[0];
-        int end1 = newInterval[1];
-
+    vector<vector<int>> insert(vector<vector<int>>& intervals,
+                               vector<int>& newInterval) {
+        vector<vector<int>> inserted;
+        bool insertion = false;
         for (int i = 0; i < intervals.size(); i++) {
-
-            int start2 = intervals[i][0];
-            int end2 = intervals[i][1];
-
-         if (end2 < start1) {
-                res.push_back({start2, end2});
+            if (intervals[i][0] > newInterval[0] && insertion == false) {
+                inserted.push_back(newInterval);
+                insertion = true;
             }
-
-            else if (start2 > end1) {
-                res.push_back({start1, end1});
-
-                start1 = start2;
-                end1 = end2;
-            }
-
-            else {
-                start1 = min(start1, start2);
+            inserted.push_back(intervals[i]);
+        }
+        if (insertion == false) {
+            inserted.push_back(newInterval);
+        }
+        vector<vector<int>> res;
+        int start1 = inserted[0][0];
+        int end1 = inserted[0][1];
+        for (int i = 0; i < inserted.size(); i++) {
+            int start2 = inserted[i][0];
+            int end2 = inserted[i][1];
+            if (end1 >= start2) {
+                start1 = start1;
                 end1 = max(end1, end2);
+            }else{
+
+            res.push_back({start1, end1});
+            start1 = start2;
+            end1 = end2;
             }
         }
-
         res.push_back({start1, end1});
-
-        return res;
+        
+    return res;
     }
 };
